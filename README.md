ea82b1f172aaabce9472d9b8070636c917b4e447
brendan <brendan.heberton@gmail.com>
2015-03-18 14:33:46 -0600

    styled example

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Big data hacking
 * [Isolines](http://benheb.github.io/geomash/isolines.html)
 * [Square Aggregates](http://benheb.github.io/geomash/squares.html)
 * [Geohash](http://benheb.github.io/geomash/)
-* [Geohash Petroleum Wells](http://benheb.github.io/petrol-geohash.html)
+* [Geohash Petroleum Wells](http://benheb.github.io/geomash/petrol-geohash.html)
--- hash/index.js
+++ hash/index.js
@@ -6,11 +6,16 @@ var grid = {};
  
 data.features.forEach(function(f,i){
   var ghash = geohash.encode(f.geometry.coordinates[1], f.geometry.coordinates[0],9);
+  //console.log('f.prop', f.properties);
   if (!grid[ghash]){
-    grid[ghash] = 0;
+    grid[ghash] = {
+      count: 0,
+      avg: 0
+    };
   }
-  grid[ghash]++;
+  grid[ghash].count++;
+  grid[ghash].avg = (f.properties.INTENSITY + grid[ghash].avg) / grid[ghash].count;
 });
  
 console.log(Object.keys(grid).length);
-fs.writeFileSync('geohash.out.json', JSON.stringify(grid));
+fs.writeFileSync('geohash-attrs.out.json', JSON.stringify(grid));
