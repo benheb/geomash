2e2cd55489e61d181f41021f223908e22328ebbd
brendan <brendan.heberton@gmail.com>
2015-03-18 11:35:55 -0600

    Update 2015-03-18T17:35:53.299Z

--- scripts/isoline.js
+++ scripts/isoline.js
@@ -13,13 +13,13 @@ L.esri.basemapLayer("Gray").addTo(map);
 //}).addTo(map);
 
 function getColor(d) {
-  //console.log('d', d);
-  return d > 500 ? '#800026' :
-         d > 300  ? '#BD0026' :
-         d > 100  ? '#E31A1C' :
-         d > 50  ? '#FC4E2A' :
-         d > 20   ? '#FD8D3C' :
-         d > 10   ? '#FEB24C' :
+  console.log('d', d);
+  return d > 55000 ? '#800026' :
+         d > 51000  ? '#BD0026' :
+         d > 48000  ? '#E31A1C' :
+         d > 44000  ? '#FC4E2A' :
+         d > 43000   ? '#FD8D3C' :
+         d > 41000   ? '#FEB24C' :
          d > 0   ? '#FED976' :
                     'none';
 }
@@ -27,19 +27,16 @@ function getColor(d) {
 
 function styler(feature) {
   return {
-    fillColor: getColor(feature.properties.pt_count),
-    weight: 0.2,
+    weight: 0.7,
     opacity: 1,
-    color: '#EEE',
+    color: getColor(feature.properties.INTENSITY),
     fillOpacity: 0.7
   };
 }
 
 $.ajax({
   dataType: "json",
-  //url: "/data/intensity.json",
-  //url: "/data/count.json",
-  url: "data/count-large.json",
+  url: "data/intensity.json",
   success: function(data) {
     console.log('data', data);
     L.geoJson(data, {
